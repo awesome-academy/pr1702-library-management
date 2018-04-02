3e7d55609dea5934f5fa0bcf6eafc43373b9a4f4
giangmt <maitruonggiangbkhn@gmail.com>
2018-04-02 15:02:46 +0700

    login 2

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
   root "books#index"
 
+  get "/signup", to: "users#new"
+  post "/signup", to: "users#create"
+  get "/login", to: "sessions#new"
+  post "/login", to: "sessions#create"
+  delete "/logout", to: "sessions#destroy"
   get "static_pages/home"
   get "static_pages/help"
   resources :books
+  resources :users
 end
